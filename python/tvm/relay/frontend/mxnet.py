f3f6836c2c59b886a470970574447e1677578a7d
Leyuan Wang <laurawly@gmail.com>
2019-03-20 18:08:33 +0000

    argsort added to realy

--- python/tvm/relay/frontend/mxnet.py
+++ python/tvm/relay/frontend/mxnet.py
@@ -213,7 +213,7 @@ def _mx_slice_axis(inputs, attrs):
     ax_end = attrs.get_str("end")
     if axis < 0:
         axis += len(shape)
-    assert 0 <= axis < len(shape)
+    assert axis >= 0 and axis < len(shape)
     if ax_end == "None":
         ax_end = int(shape[axis])
     else:
@@ -222,8 +222,8 @@ def _mx_slice_axis(inputs, attrs):
         ax_beg += int(shape[axis])
     if ax_end < 0:
         ax_end += int(shape[axis])
-    assert 0 <= ax_beg < int(shape[axis])
-    assert ax_beg < ax_end <= int(shape[axis])
+    assert ax_beg >= 0 and ax_beg < int(shape[axis])
+    assert ax_end > ax_beg and ax_end <= int(shape[axis])
     begin = []
     end = []
     for i, dim in enumerate(shape):
@@ -429,6 +429,14 @@ def _mx_reverse(inputs, attrs):
     return _op.reverse(inputs[0], **new_attrs)
 
 
+def _mx_argsort(inputs, attrs):
+    assert len(inputs) == 1
+    new_attrs = {}
+    new_attrs["axis"] = attrs.get_int("axis", -1)
+    new_attrs["is_ascend"] = attrs.get_bool("is_ascend", True)
+    return _op.argsort(inputs[0], **new_attrs)
+
+
 def _mx_roi_align(inputs, attrs):
     new_attrs = {}
     new_attrs["pooled_size"] = attrs.get_int_tuple("pooled_size")
@@ -436,6 +444,7 @@ def _mx_roi_align(inputs, attrs):
     new_attrs["sample_ratio"] = attrs.get_int("sample_ratio", -1)
     new_attrs["layout"] = "NCHW"
     return _op.vision.roi_align(inputs[0], inputs[1], **new_attrs)
+
 
 def _mx_resize(inputs, attrs):
     scale_height = attrs.get_float("scale_height", None)
@@ -524,53 +533,11 @@ def _mx_shape_array(inputs, attrs):
     return _op.shape_of(inputs[0], dtype='int64')
 
 
-def _mx_full(inputs, attrs):
-    assert len(inputs) == 0
-    val = attrs.get_float("value")
-    shape = attrs.get_int_tuple("shape")
-    dtype = attrs.get_str("dtype", "float32")
-    return _op.full(_expr.const(val, dtype), shape, dtype)
-
-
-def _mx_squeeze(inputs, attrs):
-    assert len(inputs) == 1
-    axis = attrs.get_int_tuple("axis", None)
-    return _op.squeeze(inputs[0], axis)
-
-
-def _mx_broadcast_axis(inputs, attrs):
-    assert len(inputs) == 1
-    axis = attrs.get_int_tuple("axis", [])
-    size = attrs.get_int_tuple("size", [])
-    assert len(axis) == len(size)
-    if len(axis) == 0:
-        return inputs[0]
-    src_shape = ir_pass.infer_type(inputs[0])._checked_type_.shape
-    tgt_shape = []
-    for i, dim in enumerate(src_shape):
-        if i not in axis:
-            tgt_shape.append(dim)
-        else:
-            assert int(dim) == 1
-            idx = axis.index(i)
-            tgt_shape.append(size[idx])
-    return _op.broadcast_to(inputs[0], tgt_shape)
-
-
-def _mx_embedding(inputs, _):
-    assert len(inputs) == 2
-    indices, weight = inputs
-    return _op.take(weight, indices.astype('int32'), axis=0)
-
-
 # Note: due to attribute conversion constraint
 # ops in the identity set must be attribute free
 _identity_list = [
     "log",
     "exp",
-    "sqrt",
-    "floor",
-    "ceil",
     "sigmoid",
     "tanh",
     "negative",
@@ -606,6 +573,7 @@ _convert_map = {
     "Flatten"                : _rename(_op.nn.batch_flatten),
     # scalar power
     "square"                 : _mx_make_power(2),
+    "sqrt"                   : _mx_make_power(1/2),
     "rsqrt"                  : _mx_make_power(-1/2),
     "cbrt"                   : _mx_make_power(1/3),
     "rcbrt"                  : _mx_make_power(-1/3),
@@ -687,15 +655,12 @@ _convert_map = {
     "batch_dot"     : _mx_batch_dot,
     "LeakyReLU"     : _mx_leaky_relu,
     "_arange"       : _mx_arange,
-    "_full"         : _mx_full,
     "repeat"        : _mx_repeat,
     "tile"          : _mx_tile,
+    "argsort"       : _mx_argsort,
     "reverse"       : _mx_reverse,
-    "squeeze"       : _mx_squeeze,
-    "broadcast_axis": _mx_broadcast_axis,
     "BlockGrad"     : _mx_BlockGrad,
     "shape_array"   : _mx_shape_array,
-    "Embedding"     : _mx_embedding,
     "SoftmaxOutput" : _mx_softmax_output,
     "SoftmaxActivation" : _mx_softmax_activation,
     # vision
